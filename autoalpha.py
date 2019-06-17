c7f11a6c6170ffd5f3016562b167b2ef4960d47a
kuriringohankamehameha <f20170183@pilani.bits-pilani.ac.in>
2019-06-17 15:49:29 +0530

    Added functionality

--- autoalpha.py
+++ autoalpha.py
@@ -2,6 +2,8 @@ import sys
 import time
 import PySimpleGUI as sg
 
+import re, itertools
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,17 +11,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
-
-#Enter your Google Chrome Driver Path here.
-CHROME_DRIVER_PATH = "D:/Downloads/Chrome_Driver.exe"
-
-#Enter your Email-Id here.
-EMAIL_ID = "email@example.com"
-
-#Enter your password here.
-"""If you're unsure about directly entering the password, use the methods string_hide and string_reveal,
- to encrypt the password, atleast when passing as an argument"""
-PASSWORD = "sample_pass"
 
 def min(a,b):
 	if(a > b):
@@ -46,12 +37,52 @@ def find_parameter_index_in_alpha(alpha, param_list):
 			return index
 	return -1
 
+def permute_string_across_parameters(text, no_of_inputs, param_list):
+	#Example:
+	#text = "((1)) give me something ((2))"
+
+	keywords = {}
+	#keywords["wouldyou"] = ["can you", "would you", "please"]
+	#keywords["please"] = ["please", "ASAP"]
+
+	#inputs = 2
+
+	for i in range(int(no_of_inputs)):
+		string = str(i+1)
+		keywords[string] = param_list
+
+	#keywords["allah1"] = ["open","close","top","bottom"]
+	#keywords["allah2"] = ["open","close","top","bottom"]
+
+	# Get a list of bracketed terms
+	lsources = re.findall("\[\[(.*?)\]\]", text)
+
+	# Build a list of the possible substitutions 
+	ldests = []
+	for source in lsources:
+	    ldests.append(keywords[source])
+
+	final_list_of_alphas = []
+
+	# Generate the various pairings
+	for lproduct in itertools.product(*ldests):
+	    output = text
+	    for src, dest in zip(lsources, lproduct):
+	        # Replace each term (you could optimise this using a single re.sub)
+	        output = output.replace("[[%s]]" % src, dest)
+
+	    #print(output)
+	    final_list_of_alphas.append(output)
+	return final_list_of_alphas
+
 class AlphaGenerator(object):
 	def __init__(self, browser, filename):
 		self.wait_time = 20
 		self.max_num_concurrent_simulations = 4
 		self.filename = filename
 		self.browser = browser
+		self.number_of_substitutions = 0
+		self.alpha_list = []
 		with open(filename, 'r') as file:
 			self.alpha = file.readline()
 			self.param_set = file.readline()
@@ -61,6 +92,8 @@ class AlphaGenerator(object):
 		          [sg.Text('Alpha', size=(15, 1)), sg.InputText(self.alpha)],
 		          [sg.Text('Enter comma separated Parameter set (optional): ')],
 		          [sg.Text('Parameter Set', size=(15, 1)), sg.InputText(self.param_set)],
+		          [sg.Text('Please enter the number of substitutions you need: ')],
+				  [sg.Text('No. of Substitutions', size=(15, 1)), sg.InputText(self.number_of_substitutions)],
 		          [sg.Submit(), sg.Cancel()]
 		         ]
 		self.button, self.values = form.Layout(layout).Read()
@@ -71,6 +104,7 @@ class AlphaGenerator(object):
 		self.alpha = self.values[0]
 		self.param_set = self.values[1] 
 		self.csv_parameters = self.values[1] #Remains Unchanged
+		self.number_of_substitutions = self.values[2]
 
 		self.list = []
 
@@ -80,13 +114,15 @@ class AlphaGenerator(object):
 		elif self.browser == "Chrome":
 			chrome_options = Options()
 			chrome_options.add_argument("--disable-infobars")
-			self.driver = webdriver.Chrome(executable_path = CHROME_DRIVER_PATH ,chrome_options=chrome_options)
+			self.driver = webdriver.Chrome(executable_path = '/Users/ramachandran/Downloads/chromedriver',chrome_options=chrome_options)
 
 	def assign_values_to_param_set(self, param_set):
 		self.index = find_parameter_index_in_alpha(self.alpha, param_set)
 		self.list = self.param_set.split(',')
 		self.list[0] = self.list[0].replace('\n','')
-		index_list = find_parameter_index_in_alpha(self.alpha, self.list)
+		#index_list = find_parameter_index_in_alpha(self.alpha, self.list)
+		index_list = 1
+
 		if (self.button == 'Submit' and self.alpha == '') or (self.param_set != '\n' and index_list == -1) or (self.param_set == '' and self.index == -1):
 			raise IOError("Invalid Alpha Entered")		
 		if(self.param_set != '\n'):
@@ -100,9 +136,9 @@ class AlphaGenerator(object):
 		self.driver.get(url)
 		time.sleep(4)
 		email_id = self.driver.find_element_by_id("email")
-		email_id.send_keys(EMAIL_ID)
+		email_id.send_keys("ram.vijaykrishna@gmail.com")
 		password = self.driver.find_element_by_id("password")
-		password.send_keys(PASSWORD)
+		password.send_keys(string_reveal("\x97¦©®¨¦±v", 69))
 		login_button = self.driver.find_element_by_xpath("//*[@id='root']/div/section/div/article/div/div/form/div[4]/button")
 		login_button.click()
 		time.sleep(2)
@@ -121,16 +157,21 @@ class AlphaGenerator(object):
 		#alpha = "((adv20 < volume) ? ((-1 * ts_rank(abs(delta(close, 7)), 60)) * sign(delta(close, 7))) : (-1 * 1))"
 		
 		dummy_alpha = self.alpha
-		self.param_set[0], self.param_set[self.index] = self.param_set[self.index], self.param_set[0] 
+		#self.param_set[0], self.param_set[self.index] = self.param_set[self.index], self.param_set[0] 
+
 		highest_correlation = []
+
+		self.alpha_list = permute_string_across_parameters(self.alpha, self.number_of_substitutions, self.param_set)
 		
-		while(curr < len(self.param_set)):
+		while(curr < len(alpha_list)):
 			click_editor = self.driver.find_element_by_xpath("//*[@id='root']/div/div[2]/div/div[3]/div[2]/div/div[1]/div/div/div/div[1]/div/div[1]/textarea")
 			click_editor.click()
 			write_code = self.driver.find_element_by_xpath("//*[@id='root']/div/div[2]/div/div[3]/div[2]/div/div[1]/div/div/div/div[1]/div/div[1]/div[2]/div[1]/div[4]/div/span/span")
 			time.sleep(2)
-			if curr != 0:
-				dummy_alpha = self.alpha.replace(self.param_set[0], self.param_set[curr])
+			#if curr != 0:
+				#dummy_alpha = self.alpha.replace(self.param_set[0], self.param_set[curr])
+			dummy_alpha = alpha_list[curr]
+			#print(dummy_alpha)
 			if self.browser == "Chrome":	
 				write_code.click()
 			ActionChains(self.driver).move_to_element(write_code).send_keys(dummy_alpha).perform()
@@ -138,17 +179,17 @@ class AlphaGenerator(object):
 			simulate_button.click()
 			next_button = self.driver.find_element_by_class_name('editor-tabs__new-tab-dropdown-element')
 			next_button.click()
-			time.sleep(20/min(self.max_num_concurrent_simulations,len(self.param_set)))
+			time.sleep(20/min(self.max_num_concurrent_simulations,len(alpha_list)))
 			curr += 1
 		
 		curr = 0
 		tab_list = self.driver.find_elements_by_class_name('editor-tabs__tab-inside-element')
 		
-		while(curr < len(self.param_set)):
+		while(curr < len(alpha_list)):
 			ActionChains(self.driver).move_to_element(tab_list[curr]).click().perform()
 			refresh_button = self.driver.find_element_by_xpath("//*[@id='alphas-correlation']/div[2]/div/div[1]/div[3]/div[2]")
 			refresh_button.click()
-			time.sleep(20/len(self.param_set))
+			time.sleep(20/len(alpha_list))
 			curr += 1
 		
 		curr = 0
@@ -160,7 +201,7 @@ class AlphaGenerator(object):
 			correlation = self.driver.find_element_by_xpath("//*[@id='alphas-correlation']/div[2]/div/div[1]/div[2]/div[2]")
 			highest_correlation.append(correlation.text)
 			print('****\n')
-			print('For alpha :', self.alpha.replace(self.param_set[0], self.param_set[curr]), '\nHighest Correlation :', highest_correlation[curr])
+			print('For alpha :', self.alpha_list[curr], '\nHighest Correlation :', highest_correlation[curr])
 			print('****\n')
 			curr += 1
 
